faaf9dce8bc0d43e11f385f62a4197cb552cbe6f
RehanMalhotra <82750179+RehanMalhotra@users.noreply.github.com>
2021-07-04 12:12:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("Hello") 
+var name="Cricket"
+console.log(name)
+var num1=2
+var num2=3
+var sum1=num1 + num2
+console.log(sum1)
+var num3=4
+var num4=3
+var sum2=num3 - num4
+console.log(sum2)
+var num5=2
+var num6=3
+var sum3=num5 * num6
+console.log(sum3)
+var num7=6
+var num8=3
+var sum4=num7 / num8
+console.log(sum4)
